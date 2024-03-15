3a26ced64b0177e979593fab771cf1ecc2c1464d
Billy Osodo <66448074+Negativeice-0@users.noreply.github.com>
2024-03-15 10:13:53 +0300

    Back- to - basics
    All those tutorials seem to always have an error, back to the source with oracle getting started.

---
+++ HelloWorldApp.java
@@ -0,0 +1,15 @@
+package helloworldapp;
+
+public class HelloWorldApp {
+
+	public HelloWorldApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.");
+
+	}
+
+}
